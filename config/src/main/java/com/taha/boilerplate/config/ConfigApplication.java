7049b1d9ad6781677ed23ae69a5dfcef77c9947e
taha <shaikh.taha95@gmail.com>
2019-07-24 17:00:43 +0500

    feat: added config server
    added config server and configuration for the example service

---
+++ config/src/main/java/com/taha/boilerplate/config/ConfigApplication.java
@@ -0,0 +1,17 @@
+package com.taha.boilerplate.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+
+}
